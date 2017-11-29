6afee504b49256f794a46ac360a0d93dcf3a880e
Fatemeh <sfmodarres@gmail.com>
2017-11-29 01:18:20 -0500

    removed size as a private member of bitvector.

--- include/bitvector.h
+++ include/bitvector.h
@@ -30,7 +30,7 @@
 
 class BitVector {
 	public:
-		BitVector() : bits(0), size(0) {};
+		BitVector() : bits(0)/*, size(0)*/ {};
 		BitVector(uint64_t size);
 
 		sdsl::bit_vector get_bits() const {
@@ -44,17 +44,17 @@ class BitVector {
 		void resize(const uint64_t len);
 		uint64_t get_int(uint64_t startP, uint64_t len=64) {return bits.get_int(startP, len);}
 		bool operator==(const BitVector& b) const { return bits == b.bits; }
-		size_t size_in_bytes() {return sdsl::size_in_bytes(bits) + 8;}
+		size_t size_in_bytes() {return sdsl::size_in_bytes(bits) /*+ 8*/;}
 	private:
 		sdsl::bit_vector bits;
-		uint64_t size;
+		//uint64_t size;
 };
 
 class BitVectorRRR {
 	public:
-		BitVectorRRR() : rrr_bits(BitVector().get_bits()), size(0) {};
-		BitVectorRRR(const BitVector& bv) : rrr_bits(bv.get_bits()),
-				size(bv.bit_size()) {};
+		BitVectorRRR() : rrr_bits(BitVector().get_bits())/*, size(0)*/ {};
+		BitVectorRRR(const BitVector& bv) : rrr_bits(bv.get_bits())
+											/*,size(bv.bit_size())*/ {};
 		BitVectorRRR(std::string& filename);
 
 		bool operator[](uint64_t idx);
@@ -63,11 +63,11 @@ class BitVectorRRR {
 		uint64_t get_int(uint64_t startP, uint64_t len=64) {
 			return rrr_bits.get_int(startP, len);
 		}
-		size_t bit_size() {return size;}
-		size_t size_in_bytes() {return sdsl::size_in_bytes(rrr_bits) + 8 + 8;}
+		size_t bit_size() {return rrr_bits.size();}
+		size_t size_in_bytes() {return sdsl::size_in_bytes(rrr_bits) /*+ 8 + 8*/;}
 	private:
 		sdsl::rrr_vector<63> rrr_bits;
-		uint64_t size;
+		//uint64_t size;
 };
 
 template <class T>
--- src/bitvector.cc
+++ src/bitvector.cc
@@ -1,39 +1,39 @@
 #include "bitvector.h"
 
-BitVector::BitVector(uint64_t size) : size(size) {
+BitVector::BitVector(uint64_t size) /*: size(size)*/ {
 		bits = sdsl::bit_vector(size);
 }
 
 bool BitVector::operator[](uint64_t idx) {
-	assert(idx < size);
+	assert(idx < bits.bit_size());
 	return bits[idx];
 }
 
 void BitVector::set(uint64_t idx) {
-	assert(idx < size);
+	assert(idx < bits.bit_size());
 	bits[idx] = 1;
 }
 
 void BitVector::resize(const uint64_t len) {
 	bits.bit_resize(len);
-	size = len;
+	//size = len;
 }
 
 BitVectorRRR::BitVectorRRR(std::string& filename) {
 	sdsl::load_from_file(rrr_bits, filename);
-	size = rrr_bits.size();
-	DEBUG_CDBG("Read rrr bit vector of size " << size << " from file " <<
-						 filename);
+	//size = rrr_bits.size();
+	/*DEBUG_CDBG("Read rrr bit vector of size " << size << " from file " <<
+						 filename);*/
 }
 
 bool BitVectorRRR::operator[](uint64_t idx) {
-	assert(idx < size);
+	assert(idx < rrr_bits.size());
 	return rrr_bits[idx];
 }
 
 bool BitVectorRRR::serialize(std::string& filename) {
-	DEBUG_CDBG("Serializing rrr bit vector of size " << size << " to file " <<
-						 filename);
+	/*DEBUG_CDBG("Serializing rrr bit vector of size " << size << " to file " <<
+						 filename);*/
 	return sdsl::store_to_file(rrr_bits, filename);
 }
 
