fee14313a52d87724696d27d67d7f05f2c526592
Prashant Pandey <prashant.prashn@gmail.com>
2018-01-09 22:55:06 -0500

    more logging.

--- include/cqf.h
+++ include/cqf.h
@@ -196,8 +196,9 @@ void CQF<key_obj>::Iterator::operator++(void) {
 		aiocb.aio_buf = (volatile void*)buffer;
 		aiocb.aio_nbytes = buffer_size;
 		if ((last_prefetch_offset + buffer_size) < last_read_offset) {
-			std::cerr << "resetting.. lpo:" << last_prefetch_offset << " lro:" <<
-									 last_read_offset << std::endl;
+			if (last_prefetch_offset != 0)
+				std::cerr << "resetting.. lpo:" << last_prefetch_offset << " lro:" <<
+									 	last_read_offset << std::endl;
 			last_prefetch_offset = ( last_read_offset & ~(4095ULL) ) + buffer_size;
 		} else {
 			last_prefetch_offset += buffer_size;
