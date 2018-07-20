0b056608c49d61d62b5fa1b63b79aaf25bc5c830
Fatemeh Almodaresi <sfmodarres@gmail.com>
2018-07-20 00:39:07 -0400

    Added number of set bits as weights of 0-end edges while building the graph (before building MST)

--- src/MSF.cc
+++ src/MSF.cc
@@ -77,7 +77,7 @@ struct DisjointSets {
     DisjointSets(uint64_t n) {
         // Allocate memory
         this->n = n;
-        els.resize(n + 1);
+        els.resize(n);
         // Initially, all vertices are in
         // different sets and have rank 0.
         for (uint64_t i = 0; i <= n; i++) {
@@ -88,7 +88,7 @@ struct DisjointSets {
 
     // Find the parent of a node 'u'
     // Path Compression
-    int find(uint32_t u) {
+    uint32_t find(uint32_t u) {
         /* Make the parent of the nodes in the path
            from u--> parent[u] point to parent[u] */
         if (u != els[u].parent)
@@ -113,10 +113,13 @@ struct DisjointSets {
 
 // Structure to represent a graph
 struct Graph {
-    uint64_t V, E;
-    //vector<Edge> edges;
+
+    uint64_t V;
+
     vector<vector<Edge>> edges;
     vector<vector<EdgePtr>> mst;
+
+    uint64_t mst_totalWeight{0};
 
     Graph(uint64_t bucketCnt) { edges.resize(bucketCnt); }
 
@@ -128,14 +131,9 @@ struct Graph {
 
     // Function to find MST using Kruskal's
     // MST algorithm
-    DisjointSets kruskalMSF(uint32_t bucketCnt/*, ifstream &file*/) {
+    DisjointSets kruskalMSF(uint32_t bucketCnt) {
         int mst_wt = 0; // Initialize result
 
-        // Sort edges in increasing order on basis of cost
-
-        /*sort(edges.begin(), edges.end(),
-             [](const Edge &e1, const Edge &e2) { return e1.weight < e2.weight; });
-*/
         // Create disjoint sets
         DisjointSets ds(V);
 
@@ -169,6 +167,7 @@ struct Graph {
                     mst[v].emplace_back(bucketCntr, edgeIdxInBucket);
                     nodes[u] = 1;
                     nodes[v] = 1;
+                    mst_totalWeight += w;
                     mergeCntr++;
                 }/* else {
                             if (nodes.find(u) == nodes.end() || nodes.find(v) == nodes.end())
@@ -202,65 +201,95 @@ struct Graph {
 struct Opts {
     std::string filename;
     uint64_t numNodes; // = std::stoull(argv[2]);
-    uint64_t bucketCnt; // = std::stoull(argv[3]);
-    uint64_t numSamples;
+    uint32_t bucketCnt; // = std::stoull(argv[3]);
+    uint16_t numSamples;
     std::string eqClsListFile;
 };
 
-void loadEqs(std::string filename, std::vector<sdsl::rrr_vector < 63>> &bvs) {
-    bvs.reserve(20);
-    std::string eqfile;
-    std::ifstream eqlist(filename);
-    if (eqlist.is_open()) {
-        uint64_t accumTotalEqCls = 0;
-        while (getline(eqlist, eqfile)) {
-            sdsl::rrr_vector<63> bv;
-            bvs.push_back(bv);
-            sdsl::load_from_file(bvs.back(), eqfile);
-        }
-    }
-    //BitVectorRRR bv(eqfile);
-    std::cerr << "loaded all the equivalence classes: "
-              << ((bvs.size() - 1) * EQS_PER_SLOT + bvs.back().size())
-              << "\n";
+void loadEqs(std::string filename, std::vector<sdsl::rrr_vector < 63>>
+
+&bvs) {
+bvs.reserve(20);
+std::string eqfile;
+std::ifstream eqlist(filename);
+if (eqlist.
+
+is_open()
+
+) {
+uint64_t accumTotalEqCls = 0;
+while (
+getline(eqlist, eqfile
+)) {
+sdsl::rrr_vector<63> bv;
+bvs.
+push_back(bv);
+sdsl::load_from_file(bvs
+.
+
+back(), eqfile
+
+);
+}
+}
+//BitVectorRRR bv(eqfile);
+std::cerr << "loaded all the equivalence classes: "
+<< ((bvs.
+
+size()
+
+- 1) * EQS_PER_SLOT + bvs.
+
+back()
+
+.
+
+size()
+
+)
+<< "\n";
 }
 
-void buildColor(std::vector<sdsl::rrr_vector < 63>>&bvs, std::vector<uint64_t> &eq,
+void buildColor(std::vector<sdsl::rrr_vector < 63>> &bvs,
+std::vector<uint64_t> &eq,
         uint64_t eqid,
-        uint64_t num_samples) {
-    uint64_t i{0}, bitcnt{0}, wrdcnt{0};
-    uint64_t idx = eqid / EQS_PER_SLOT;
-    uint64_t offset = eqid % EQS_PER_SLOT;
-    //std::cerr << eqid << " " << num_samples << " " << idx << " " << offset << "\n";
-    while (i<num_samples) {
-        bitcnt = std::min(num_samples - i, (uint64_t) 64);
-        uint64_t wrd = (bvs[idx]).get_int(offset * num_samples + i, bitcnt);
-        eq[wrdcnt++] = wrd;
-        i += bitcnt;
-    }
+uint64_t num_samples
+) {
+uint64_t i{0}, bitcnt{0}, wrdcnt{0};
+uint64_t idx = eqid / EQS_PER_SLOT;
+uint64_t offset = eqid % EQS_PER_SLOT;
+//std::cerr << eqid << " " << num_samples << " " << idx << " " << offset << "\n";
+while (i<num_samples) {
+bitcnt = std::min(num_samples - i, (uint64_t) 64);
+uint64_t wrd = (bvs[idx]).get_int(offset * num_samples + i, bitcnt);
+eq[wrdcnt++] =
+wrd;
+i +=
+bitcnt;
+}
 }
 
+uint16_t sum1s(std::vector<sdsl::rrr_vector < 63>> &bvs, uint64_t eqid,
+        uint64_t num_samples) {
+    uint16_t res{0};
+    std::vector<uint64_t> eq;
+    eq.resize((uint64_t)std::ceil((double)num_samples/64.0));
+    buildColor(bvs, eq, eqid, num_samples);
+    for (uint64_t i = 0; i < eq.size(); i += 1) {
+        res += (uint16_t)sdsl::bits::cnt(eq[i]);
+    }
+    return res;
+}
 
 int main(int argc, char *argv[]) {
     /* Let us create above shown weighted
        and undirected graph */
     using namespace clipp;
     enum class mode {
-        build, ccInfo, help
+        build, help
     };
     mode selected = mode::help;
     Opts opt;
-    auto ccInfo_mode = (
-            command("ccInfo").set(selected, mode::ccInfo),
-                    required("-e", "--edge-filename") &
-                    value("edge_filename", opt.filename) % "file containing list of eq. class edges.",
-                    required("-n", "--eqCls-cnt") &
-                    value("equivalenceClass_count", opt.numNodes) % "Total number of equivalence (color) classes.",
-                    required("-b", "--bucket-cnt") &
-                    value("bucket_count", opt.bucketCnt) % "Total number of valid distances.",
-                    required("-s", "--numSamples") &
-                    value("numSamples", opt.numSamples) % "Total number of experiments (samples)."
-    );
 
     auto build_mode = (
             command("build").set(selected, mode::build),
@@ -277,7 +306,7 @@ int main(int argc, char *argv[]) {
     );
 
     auto cli = (
-            (build_mode | ccInfo_mode | command("help").set(selected, mode::help)
+            (build_mode | command("help").set(selected, mode::help)
             )
     );
 
@@ -290,170 +319,164 @@ int main(int argc, char *argv[]) {
         std::cout << make_man_page(cli, "MSF");
         return 1;
     }
-
-    //explore_options_verbose(res);
-
-    if (res) {
-        switch (selected) {
-            //case mode::ccInfo: query_main(qopt);  break;
-            //case mode::build: validate_main(vopt);  break;
-            case mode::help:
-                std::cerr << make_man_page(cli, "MSF");
-                break;
-        }
+    if (!res) {
+        std::cerr << "Cannot parse the input arguments\n";
+        std::exit(1);
     }
+    if (selected == mode::help) {
+        std::cerr << make_man_page(cli, "MSF");
+        std::exit(1);
+    }
+    //explore_options_verbose(res);
 
     std::cerr << "here are the inputs: \n"
               << opt.filename << "\n"
               << opt.numNodes << "\n"
               << opt.bucketCnt << "\n";
     opt.numNodes++; // number of nodes is one more than input including the zero node
-    ifstream file(opt.filename);
 
+    std::cerr << "Loading all the equivalence classes first .. \n";
+    std::vector<sdsl::rrr_vector<63>> eqs;
+    eqs.reserve(20);
+    loadEqs(opt.eqClsListFile, eqs);
+    std::cerr << "Done loading list of equivalence class buckets\n";
+
+    ifstream file(opt.filename);
     Graph g(opt.bucketCnt);
 
-    uint32_t w_;
+    uint16_t w_;
     uint32_t n1, n2, edgeCntr{0}, zero{(uint32_t) opt.numNodes - 1};
-    std::string tmp;
     {
-        //unordered_set<uint64_t> nodes;
-        sdsl::bit_vector nodes(opt.numNodes, 0);
-        std::getline(file, tmp);
-        for (uint64_t i = 0; i < opt.numNodes; i++) {
-            g.addEdge(zero, i, opt.numSamples);
+        std::cerr << "Adding edges from 0 to each node with number of set bits in the node as weight .. \n";
+        for (uint32_t i = 0; i < opt.numNodes; i++) {
+            uint16_t ones = sum1s(eqs, i, opt.numSamples);
+            g.addEdge(zero, i, ones);
         }
+        std::cerr << "Done adding 0-end edges.\n";
+        std::cerr << "Adding edges between color classes .. \n";
         while (file.good()) {
             file >> n1 >> n2 >> w_;
             g.addEdge(n1, n2, w_);
-            nodes[n1] = 1;
-            nodes[n2] = 1;
             edgeCntr++;
-            //nodes.insert(n1);
-            //nodes.insert(n2);
         }
         //file.clear();
         //file.seekg(0, ios::beg);
         file.close();
+        std::cerr << "Done adding edges between color classes .. \n";
 
-        uint64_t distinctNodes{0};
-        for (uint64_t i = 0; i < nodes.size(); i += 64) {
-            distinctNodes += sdsl::bits::cnt(nodes.get_int(i, 64));
-        }
-        std::cerr << "# of nodes: " << distinctNodes//nodes.size()
-                  << "\n# of edges: " << edgeCntr
+        std::cerr << "\n# of edges: " << edgeCntr
                   << "\n";
 //        nodes.clear();
     }
     g.V = opt.numNodes;
     g.mst.resize(opt.numNodes);
-    g.E = edgeCntr;
     //g.V = numNodes;
     //ifstream file(filename);
 
-    DisjointSets ds = g.kruskalMSF(opt.bucketCnt/*, file*/);
+    DisjointSets ds = g.kruskalMSF(opt.bucketCnt);
+    std::queue<uint32_t> q;
+    vector<vector<Child>> p2c(opt.numNodes);
+    vector<uint32_t> c2p(opt.numNodes);
 
-    if (selected == mode::ccInfo) {
-        for (auto &el : ds.els) {
-            if (el.w != 0) {
-                std::cout << el.edges << "\t" << el.w << "\t" << el.rnk << "\n";
+    for (auto e = 0; e < g.mst.size(); e++) {
+        // put all the mst leaves into the queue
+        if (g.mst[e].size() == 1) {
+            q.push(e); // e is a leaf
+        }
+    }
+    // now go run the algorithm to find the root of the tree and all the edge directions
+    uint32_t root;
+    bool check = false;
+    uint64_t nodeCntr{0};
+    while (!q.empty()) {
+        uint32_t node = q.front();
+        q.pop();
+        if (g.mst[node].size() == 0) {
+            // this node is the root
+            // and this should be the end of the loop
+            // just as a validation, I'll continue and expect the while to end here
+            if (check) {
+                std::cerr << "A TERRIBLE BUG!!!\n"
+                          << "finding a node with no edges should only happen once at the root\n";
+                std::exit(1);
             }
+            check = true;
+            root = node;
+            continue;
+        }
+        // what ever is in q (node) is a leaf and has only one edge left
+        // fetch the pointer to that edge (bucket+idx)
+        auto buck = g.mst[node][0].bucket;
+        auto idx = g.mst[node][0].idx;
+        // fetch the two ends of the edge and based on the node id decide which one is the src/dest
+        // Destination is the one that represents current node which has been selected as leaf sooner than the other end
+        uint64_t dest = g.edges[buck][idx].n1;
+        uint64_t src = g.edges[buck][idx].n2;
+        if (src == node) { // swap src & dest since src is the leaf
+            std::swap(src, dest);
+        }
+        p2c[src].emplace_back(dest, g.edges[buck][idx].weight);
+        // erase the edge from src list of edges
+        g.mst[dest].erase(std::remove_if(
+                g.mst[dest].begin(), g.mst[dest].end(),
+                [buck, idx](const EdgePtr &x) {
+                    return x.bucket == buck && x.idx == idx;
+                }), g.mst[dest].end());
+        // erase the edge from dest list of edges
+        g.mst[src].erase(std::remove_if(
+                g.mst[src].begin(), g.mst[src].end(),
+                [buck, idx](const EdgePtr &x) {
+                    return x.bucket == buck && x.idx == idx;
+                }), g.mst[src].end());
+        // the destination has no edges left
+        // but if the src has turned to a leaf (node with one edge) after removal of this last edge
+        // add it to the queue
+        if (g.mst[src].size() == 1) {
+            q.push(src);
+        }
+        nodeCntr++; // just a counter for the log
+        if (nodeCntr % 10000000 == 0) {
+            std::cerr << nodeCntr << " nodes processed toward root\n";
         }
     }
 
-    if (selected == mode::build) {
-        std::queue<uint32_t> q;
-        vector<vector<Child>> p2c(opt.numNodes);
-        vector<uint32_t> c2p(opt.numNodes);
-
-        for (auto e = 0; e < g.mst.size(); e++) {
-            // put all the mst leaves into the queue
-            if (g.mst[e].size() == 1) {
-                q.push(e); // e is a leaf
-            }
+    // calculate all the stats!!
+    // create the data structures
+    std::cerr << "Calculate Stats .. \n";
+    std::cerr << "Sum of MST weights: " << g.mst_totalWeight << "\n";
+    nodeCntr = 0;
+    std::queue<Path> pq;
+    pq.push(Path(root, 0, 0));
+    double avgDegree{0};
+    uint64_t internalNodeCnt{0};
+    std::cout << "-1\t" << root << "\t" << 0 << "\t" << 0 << "\t" << 0 << "\n";
+    while (!pq.empty()) {
+        Path p = pq.front();
+        pq.pop();
+        avgDegree += p2c[p.id].size();
+        if (p2c[p.id].size() != 0) {
+            internalNodeCnt++;
         }
-        // now go run the algorithm to find the root of the tree and all the edge directions
-        uint32_t root;
-        bool check = false;
-        uint64_t nodeCntr{0};
-        while (!q.empty()) {
-            uint32_t node = q.front();
-            q.pop();
-            if (g.mst[node].size() == 0) {
-                // this node is the root
-                // and this should be the end of the loop
-                // just to confirm this, I'll continue
-                if (check) {
-                    std::cerr << "A TERRIBLE BUG!!!\n"
-                              << "finding a node with no edges should only happen once at the root\n";
-                    std::exit(1);
-                }
-                check = true;
-                root = node;
-                continue;
-            }
-            auto buck = g.mst[node][0].bucket;
-            auto idx = g.mst[node][0].idx;
-            uint64_t dest = g.edges[buck][idx].n1;
-            uint64_t src = g.edges[buck][idx].n2;
-            if (src == node) { // swap src & dest since src is the leaf
-                std::swap(src, dest);
-            }
-            p2c[src].emplace_back(dest, g.edges[buck][idx].weight);
-            g.mst[dest].erase(std::remove_if(
-                    g.mst[dest].begin(), g.mst[dest].end(),
-                    [buck, idx](const EdgePtr &x) {
-                        return x.bucket == buck && x.idx == idx;
-                    }), g.mst[dest].end());
-            g.mst[src].erase(std::remove_if(
-                    g.mst[src].begin(), g.mst[src].end(),
-                    [buck, idx](const EdgePtr &x) {
-                        return x.bucket == buck && x.idx == idx;
-                    }), g.mst[src].end());
-            if (g.mst[src].size() == 1) {
-                q.push(src);
-            }
-            nodeCntr++;
-            if (nodeCntr % 10000000 == 0) {
-                std::cerr << nodeCntr << " nodes processed toward root\n";
-            }
+        for (auto &c : p2c[p.id]) {
+            Path cp(c.id, p.steps + 1, p.weight + c.weight);
+            std::cout << p.id << "\t"
+                      << cp.id << "\t"
+                      << cp.steps << "\t"
+                      << c.weight << "\t"
+                      << cp.weight << "\n";
+            pq.push(cp);
         }
-
-        // calculate all the stats!!
-        // create the data structures
-        std::cerr << "Calculate Stats .. \n";
-        nodeCntr = 0;
-        std::queue<Path> pq;
-        pq.push(Path(root, 0, 0));
-        double avgDegree{0};
-        uint64_t internalNodeCnt{0};
-        std::cout << "-1\t" << root << "\t" << 0 << "\t" << 0 << "\t" << 0 << "\n";
-        while (!pq.empty()) {
-            Path p = pq.front();
-            pq.pop();
-            avgDegree += p2c[p.id].size();
-            if (p2c[p.id].size() != 0) {
-                internalNodeCnt++;
-            }
-            for (auto &c : p2c[p.id]) {
-                Path cp(c.id, p.steps + 1, p.weight + c.weight);
-                std::cout << p.id << "\t"
-                          << cp.id << "\t"
-                          << cp.steps << "\t"
-                          << c.weight << "\t"
-                          << cp.weight << "\n";
-                pq.push(cp);
-            }
-            nodeCntr++;
-            if (nodeCntr % 10000000 == 0) {
-                std::cerr << nodeCntr << " nodes processed from root\n";
-            }
+        nodeCntr++;
+        if (nodeCntr % 10000000 == 0) {
+            std::cerr << nodeCntr << " nodes processed from root\n";
         }
-
-        std::cerr << "internal node count: " << internalNodeCnt << "\n"
-                  << "total out degree: " << avgDegree << "\n"
-                  << "average degree: " << avgDegree/internalNodeCnt << "\t" << avgDegree/opt.numNodes << "\n";
-
     }
+
+    std::cerr << "Sum of MST weights: " << g.mst_totalWeight << "\n"
+              << "internal node count: " << internalNodeCnt << "\n"
+              << "total out degree: " << avgDegree << "\n"
+              << "average degree: " << avgDegree / internalNodeCnt << "\t" << avgDegree / opt.numNodes << "\n";
+
 
     return 0;
 }
