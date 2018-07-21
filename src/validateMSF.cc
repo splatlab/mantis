198c459ec1a0498d44742b2e12cf4ec3df5c6569
Fatemeh Almodaresi <sfmodarres@gmail.com>
2018-07-21 01:50:25 -0400

    make validator more than just a validator! We can now evaluate # of steps required in addition to validate the encoding gonna add query timing to it soon too.

--- src/validateMSF.cc
+++ src/validateMSF.cc
@@ -83,46 +83,124 @@ public:
     }
 };
 
+uint32_t recursiveSteps(uint32_t idx, sdsl::int_vector<> &parentbv,
+                        std::vector<uint32_t> &steps,
+                        uint32_t zero) {
+    if (idx == zero)
+        return steps[idx]; // 0
+    if (steps[idx] != 0)
+        return steps[idx];
+    if (parentbv[idx] == idx) {
+        steps[idx]=1; // to retrieve the representative
+        return steps[idx];
+    }
+    steps[idx] = recursiveSteps(parentbv[idx], parentbv, steps, zero)+1;
+    return steps[idx];
+}
+
+struct Opts {
+    std::string indexDir;
+    uint64_t numSamples;
+    std::string eqlistfile;
+};
 
 int main(int argc, char *argv[]) {
-    std::string indexDir = argv[1];
-    std::string eqlistfile = argv[2];
-    uint64_t numSamples = std::stoull(argv[3]);
+
+    using namespace clipp;
+    enum class mode {
+        validate, steps, query, help
+    };
+    mode selected = mode::help;
+    Opts opt;
+
+    auto validate_mode = (
+            command("validate").set(selected, mode::validate),
+                    required("-i", "--indexDir") &
+                    value("index_dir", opt.indexDir) % "Directory containing index files.",
+                    required("-eq", "--eqCls-lst-file") &
+                    value("eqCls_list_filename", opt.eqlistfile) % "File containing list of equivalence (color) classes.",
+                    required("-s", "--numSamples") &
+                    value("numSamples", opt.numSamples) % "Total number of experiments (samples)."
+    );
+
+    auto steps_mode = (
+            command("steps").set(selected, mode::steps),
+                    required("-i", "--indexDir") &
+                    value("index_dir", opt.indexDir) % "Directory containing index files.",
+                    required("-s", "--numSamples") &
+                    value("numSamples", opt.numSamples) % "Total number of experiments (samples)."
+    );
+
+    auto cli = (
+            (validate_mode | steps_mode | command("help").set(selected, mode::help)
+            )
+    );
+
+    decltype(parse(argc, argv, cli)) res;
+    try {
+        res = parse(argc, argv, cli);
+    } catch (std::exception &e) {
+        std::cout << "\n\nParsing command line failed with exception: " << e.what() << "\n";
+        std::cout << "\n\n";
+        std::cout << make_man_page(cli, "MSF");
+        return 1;
+    }
+    if (!res) {
+        std::cerr << "Cannot parse the input arguments\n";
+        std::exit(1);
+    }
+    if (selected == mode::help) {
+        std::cerr << make_man_page(cli, "MSF");
+        std::exit(1);
+    }
+
+    std::string indexDir = opt.indexDir;
+    std::string eqlistfile = opt.eqlistfile;
+    uint64_t numSamples = opt.numSamples;
     uint64_t numWrds = (uint64_t)std::ceil((double)numSamples/64.0);
     MSFQuery query(numSamples);
     query.loadIdx(indexDir);
     uint64_t eqCount = query.parentbv.size()-1;
     std::cerr << "total # of equivalence classes is : " << eqCount << "\n";
 
-    eqvec bvs;
-    loadEqs(eqlistfile, bvs);
+    if (selected == mode::validate) {
+        eqvec bvs;
+        loadEqs(eqlistfile, bvs);
+        // choose 1M random eq classes
+        /*std::unordered_set<uint64_t> ids;
+        while (ids.size() < 1000000) {
+            ids.insert(rand() % eqCount);
+        }*/
 
-    // choose 1M random eq classes
-    std::unordered_set<uint64_t> ids;
-    /*while (ids.size() < 1000000) {
-        ids.insert(rand() % eqCount);
-    }*/
-   //ids.insert(12237996);
+        uint64_t cntr{0};
+        for (uint64_t idx = 0; idx < eqCount; idx++) {
+            std::vector<uint64_t> newEq = query.buildColor(idx, bvs);
+            std::vector<uint64_t> oldEq(numWrds);
+            buildColor(bvs, oldEq, idx, numSamples);
 
-    uint64_t cntr{0};
-    for (uint64_t idx = 0; idx < eqCount; idx++) {
-        std::vector<uint64_t> newEq = query.buildColor(idx, bvs);
-        std::vector<uint64_t> oldEq(numWrds);
-        buildColor(bvs, oldEq, idx, numSamples);
-
-        if (newEq != oldEq) {
-            std::cerr << "AAAAA! LOOOSER!!\n";
-            std::cerr << cntr << ": index=" << idx << "\n";
-            std::cerr << "new size: " << newEq.size() << " old size: " << oldEq.size() << "\n";
-            for (auto k = 0; k < newEq.size(); k++) {
-                std::cerr << newEq[k] << " " << oldEq[k] << "\n";
+            if (newEq != oldEq) {
+                std::cerr << "AAAAA! LOOOSER!!\n";
+                std::cerr << cntr << ": index=" << idx << "\n";
+                std::cerr << "new size: " << newEq.size() << " old size: " << oldEq.size() << "\n";
+                for (auto k = 0; k < newEq.size(); k++) {
+                    std::cerr << newEq[k] << " " << oldEq[k] << "\n";
+                }
+                std::exit(1);
             }
-            std::exit(1);
+            cntr++;
+            if (cntr % 10000000 == 0) {
+                std::cerr << cntr << " eqs were the same\n";
+            }
         }
-        cntr++;
-        if (cntr % 10000000 == 0) {
-            std::cerr << cntr << " eqs were the same\n";
+        std::cerr << "WOOOOW! Validation passed\n";
+    } else if (selected == mode::steps) {
+        uint32_t zero = query.parentbv.size()-1;
+        std::vector<uint32_t> steps(eqCount, 0);
+
+        for (uint64_t idx = 0; idx < eqCount; idx++) {
+            std::cout << query.parentbv[idx] << "\t"
+                      << idx << "\t"
+                      << recursiveSteps(idx, query.parentbv, steps, zero) << "\n";
         }
     }
-    std::cerr << "WOOOOW! Validation passed\n";
 }
