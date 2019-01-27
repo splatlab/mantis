3a872fcb1e987c3c766f2068ec4da0440d9610d1
Rob Johnson <robj@vmware.com>
2019-01-27 09:44:59 +0100

    try to add error checking to limitMem.sh

--- scripts/limitMem.sh
+++ scripts/limitMem.sh
@@ -9,15 +9,47 @@ GB=$1
 case $GB in
   ''|*[!0-9]*)
     echo "USAGE:"
-    echo "Limit mantis memory usage to 32GB:"
-    echo " $0 32 /mantis/command line"
+    echo "Limit a program's memory usage to <n> GB:"
+    echo " $0 <n> <program command line>"
     exit
 esac
 
 shift
 MEM=$[GB*1024*1024*1024]
 
-sudo apt install -y cgroup-tools libssl-dev
+CGCONFIGPARSER=`which cgconfigparser`
+if [ ! -n "$CGCONFIGPARSER" ]; then
+    for guess in /sbin /usr/sbin /usr/local/sbin; do
+        if [ ! -x $guess/cgconfigparser ]; then
+            CGCONFIGPARSER=$guess/cgconfigparser
+            break
+        fi
+    done
+fi
+if [ ! -n "$CGCONFIGPARSER" ]; then
+    echo "This script requires cgconfigparser."
+    echo "Try installing the cgroup-tools package."
+    echo "On Debian-based systems, such as Ubuntu:"
+    echo "  sudo apt install -y cgroup-tools libssl-dev"
+    exit 1
+fi
+
+CGEXEC=`which cgexec`
+if [ ! -n "$CGEXEC" ]; then
+    for guess in /sbin /usr/sbin /usr/local/sbin; do
+        if [ ! -x $guess/cgexec ]; then
+            CGEXEC=$guess/cgexec
+            break
+        fi
+    done
+fi
+if [ ! -n "$CGEXEC" ]; then
+    echo "This script requires cgexec."
+    echo "Try installing the cgroup-tools package."
+    echo "On Debian-based systems, such as Ubuntu:"
+    echo "  sudo apt install -y cgroup-tools libssl-dev"
+    exit 1
+fi
 
 cat <<EOF > cgconfig.conf
 mount {
@@ -42,7 +74,29 @@ group mantis {
 EOF
 
 sudo umount /var/cgroups
-sudo cgconfigparser -l cgconfig.conf
+# No error check here.
+
+sudo $CGCONFIGPARSER -l cgconfig.conf
+if [ $? ]; then
+    echo "Failed to setup the mantis cgroup"
+    exit 1
+fi
+
 echo $MEM | sudo tee /var/cgroups/mantis/memory.limit_in_bytes
+MEMCHECK=`cat /var/cgroups/mantis/memory.limit_in_bytes`
+if [ $MEM != $MEMCHECK ]; then
+    echo "Failed to set memory limit."
+    exit 1
+fi
+
 echo 3 | sudo tee /proc/sys/vm/drop_caches
-/usr/bin/time cgexec -g memory:mantis $*
+
+/usr/bin/time $CGEXEC -g memory:mantis $*
+result=$?
+
+sudo umount /var/cgroups
+if [ $? ]; then
+    echo "Warning: failed to cleanup/unmount /var/cgroups"
+fi
+
+exit $result
