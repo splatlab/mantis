8af17bbd235aebf06e68fac4eecc6bfc04eca18f
Fatemeh Almodaresi <sfmodarres@gmail.com>
2020-03-19 01:44:05 -0400

    This is beautiful! Recursively run mantis merge up to numOfSamples < 32.

--- src/parallelMantis.cc
+++ src/parallelMantis.cc
@@ -41,7 +41,41 @@
 //#include "mantis_utils.hpp"
 //#include "mantisconfig.hpp"
 
-constexpr static uint32_t classicMaxCnt = 25;
+constexpr static uint32_t classicMaxCnt = 32;
+
+
+int recursiveConstruction(uint64_t cnt, uint64_t start, uint64_t end, std::vector<std::pair<uint64_t, std::string>>&cmds,
+                          std::string& complete_input_list, std::string& execDir, std::string& inputDir, std::string& tmpOut,
+                          uint64_t numThreads,
+                          uint64_t level) {
+    std::string sysCommand;
+    std::string tmpIn = "squeakr" + std::to_string(start) + "_" + std::to_string(end);
+    if (end-start <= classicMaxCnt) {
+        sysCommand = "head -" + std::to_string(end) + " " + complete_input_list
+                     + " | tail -" + std::to_string(end-start) + " > " + inputDir + tmpIn + ";";
+        sysCommand += "/usr/bin/time " + execDir + "/mantis build -s 20 -i " + inputDir + tmpIn
+                     + " -o " + tmpOut + tmpIn + " -t "
+                     + std::to_string(numThreads) + " > " + tmpOut + tmpIn + ".log 2>&1;";
+        sysCommand += "ls -lh " + tmpOut + tmpIn + " >> " + tmpOut + tmpIn + ".log";
+        cmds.emplace_back(level, sysCommand);
+        return EXIT_SUCCESS;
+    }
+
+    uint64_t mid = (start+end)/2;
+    recursiveConstruction(cnt, start, mid, cmds, complete_input_list,
+                          execDir, inputDir, tmpOut, numThreads, level+1);
+    recursiveConstruction(cnt, mid, end, cmds, complete_input_list,
+                          execDir, inputDir, tmpOut, numThreads, level+1);
+    sysCommand = "/usr/bin/time " + execDir + "/mantis merge -t " + std::to_string(numThreads)
+                 + " -i1 " + tmpOut  + "squeakr" + std::to_string(start) + "_" + std::to_string(mid)
+                 + " -i2 " + tmpOut  + "squeakr" + std::to_string(mid) + "_" + std::to_string(end)
+                 + " -o " + tmpOut  + tmpIn
+                 + " > " + tmpOut + tmpIn + ".log 2>&1;";
+    sysCommand += "ls -lh " + tmpOut + tmpIn + " >> " + tmpOut + tmpIn + ".log;";
+    sysCommand += "rm -rf " + tmpOut + "squeakr" + std::to_string(start) + "_" + std::to_string(mid) + ";";
+    sysCommand += "rm -rf " + tmpOut + "squeakr" + std::to_string(mid) + "_" + std::to_string(end);
+    cmds.emplace_back(level, sysCommand);
+}
 
 int construct_mantis_by_merge_main(BuildOpts &opt) {
     spdlog::logger *console = opt.console.get();
@@ -53,7 +87,6 @@ int construct_mantis_by_merge_main(BuildOpts &opt) {
         while (std::getline(infile, line)) { ++num_samples; }
         infile.clear();
         infile.close();
-//        infile.seekg(0, std::ios::beg);
         console->info("Will build mantis index over {} input experiments.", num_samples);
     } else {
         console->error("Input file {} does not exist or could not be opened.", opt.inlist);
@@ -61,17 +94,21 @@ int construct_mantis_by_merge_main(BuildOpts &opt) {
     }
 
     ctpl::thread_pool p(opt.numProcesses /* num of threads in the pool */);
-    int arr[5] = {0};
     std::vector<std::future<void>> results;
-    results.reserve(opt.numProcesses*10);
-//    std::future<void> result;
-    uint32_t scntr{0}, cntr{0};
-    std::string sysCommand, createListCommand, input_lists;
+    results.reserve(num_samples);
+    uint32_t numThreads{opt.numthreads};
+    std::string complete_input_list{opt.inlist};
 
     std::string execDir = mantis::fs::getExecutableDir();
 
     if(opt.out.back() != '/')	// Make sure it is a full directory.
         opt.out += '/';
+    if (!mantis::fs::DirExists(opt.out.c_str())) {
+        mantis::fs::MakeDir(opt.out.c_str());
+    } else if (not mantis::fs::IsDirEmpty(opt.out.c_str())){
+        std::string cmd = "rm -rf " + opt.out + "*";
+        system(cmd.c_str());
+    }
 
     auto tmpOut = opt.out + mantis::TEMP_DIR;
     if (!mantis::fs::DirExists(tmpOut.c_str())) {
@@ -81,25 +118,35 @@ int construct_mantis_by_merge_main(BuildOpts &opt) {
     if (!mantis::fs::DirExists(inputDir.c_str())) {
         mantis::fs::MakeDir(inputDir.c_str());
     }
-    while (scntr < num_samples) {
-        auto tailCntr = std::min(num_samples-scntr, classicMaxCnt);
-        input_lists = "squeakr" + std::to_string(scntr) + "_" + std::to_string(scntr + tailCntr);
-        createListCommand = "head -" + std::to_string(scntr+tailCntr) + " " + opt.inlist
-                + " | tail -" + std::to_string(tailCntr) + " > " + inputDir + input_lists;
-        sysCommand = "/usr/bin/time " + execDir + "/mantis build -s 20 -i " + inputDir + input_lists
-                     + " -o " + tmpOut + input_lists + " -t "
-                     + std::to_string(opt.numthreads) + " > " + tmpOut + input_lists + ".log 2>&1";
-        results.push_back(p.push([createListCommand, sysCommand, cntr](int) {
-            std::cerr << "\n" << cntr << "\n\t" << createListCommand << "\n\t" << sysCommand << "\n";
-//            usleep(10000000);
-            system(createListCommand.c_str());
-            system(sysCommand.c_str());
+    std::vector<std::pair<uint64_t, std::string>> cmds;
+    cmds.reserve(num_samples);
+    recursiveConstruction(num_samples, 0, num_samples, cmds, complete_input_list,
+            execDir, inputDir, tmpOut, numThreads, 0);
+    if (cmds.empty()) {
+        std::cerr << "ERROR: No command to run.\n";
+        std::exit(2);
+    }
+
+    std::sort(cmds.begin(), cmds.end(), [](auto &c1, auto &c2){
+        return c1.first > c2.first;
+    });
+    uint64_t level = cmds.begin()->first;
+    for (auto &level_cmd:cmds) {
+        if (level != level_cmd.first) {
+            for (auto &r : results) {
+                r.get();
+            }
+            results.clear();
+            level = level_cmd.first;
+        }
+        results.push_back(p.push([level_cmd](int) {
+            std::cerr << level_cmd.first << ": " << level_cmd.second << "\n\n";
+            system(level_cmd.second.c_str());
         }));
-        scntr += tailCntr;
-        cntr++;
     }
     for (auto &r : results) {
         r.get();
     }
-    std::cerr << "\n\n\n\nall done\n";
+
+    std::cerr << "\nALL DONE!\n";
 }
