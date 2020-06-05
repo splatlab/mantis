f427cbc772cffefe06e5691f0a6df152c0549787
Fatemeh Almodaresi <sfmodarres@gmail.com>
2020-06-05 14:04:48 -0400

    enhancement: Don't remove the two manti, if merging failed (makes debugging easier)

--- src/hierarchicalMantisConstructor.cc
+++ src/hierarchicalMantisConstructor.cc
@@ -72,9 +72,11 @@ int recursiveConstruction(uint64_t cnt, uint64_t start, uint64_t end, std::vecto
                  + " -i2 " + tmpOut  + "squeakr" + std::to_string(mid) + "_" + std::to_string(end)
                  + " -o " + tmpOut  + tmpIn
                  + " > " + tmpOut + std::to_string(level) + tmpIn + ".log 2>&1;";
+    sysCommand += "retVal=$?;"
+                  "if [ $retVal -eq 0 ]; then"
+                  "rm -rf " + tmpOut + "squeakr" + std::to_string(start) + "_" + std::to_string(mid) + ";";
+    sysCommand += "rm -rf " + tmpOut + "squeakr" + std::to_string(mid) + "_" + std::to_string(end) + ";fi;";
     sysCommand += "ls -lh " + tmpOut + tmpIn + " >> " + tmpOut + std::to_string(level) + tmpIn + ".log;";
-    sysCommand += "rm -rf " + tmpOut + "squeakr" + std::to_string(start) + "_" + std::to_string(mid) + ";";
-    sysCommand += "rm -rf " + tmpOut + "squeakr" + std::to_string(mid) + "_" + std::to_string(end);
     cmds.emplace_back(level, sysCommand);
 }
 
